5aa06f6a9edab797650cfbc2e60f48657eac809f
ArushiBubber <77745574+ArushiBubber@users.noreply.github.com>
2022-02-02 15:44:35 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+  
+var recognition = new SpeechRecognition();
+
+var Textbox = document.getElementById("textbox"); 
+
+function start()
+{
+    Textbox.innerHTML = ""; 
+    recognition.start();
+}
